997a49fc79a672bb4f6d1454bb4ec0a502b1b730
Tenisha Rathnasekara <44230318+dilani123@users.noreply.github.com>
2021-12-28 18:45:59 +0530

    Add files via upload

---
+++ admin/ex/suppliers.php
@@ -0,0 +1,130 @@
+
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  <?php include 'includes/navbar.php'; ?>
+  <?php include 'includes/menubar.php'; ?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Suppliers
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Suppliers</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header with-border">
+              <a href="#addnew" data-toggle="modal" class="btn btn-primary btn-sm btn-flat"><i class="fa fa-plus"></i> New</a>
+            </div>
+            <div class="box-body">
+              <table id="example1" class="table table-bordered">
+                <thead>
+                  <th>Photo</th>
+                  <th>Email</th>
+                  <th>Contact No</th>
+                  <th>Address</th>
+                  <th>Name</th>
+                  <th>Status</th>
+                  <th>Date Added</th>
+                  <th>Product Items</th>
+                  <th>Lead Time</th>
+                  <th>Tools</th>
+                </thead>
+                <tbody>
+                  <tr>
+                            <td>
+                              <img src="images/profile.jpg" height='30px' width='30px'>
+                             </td>
+                            <td>"email"</td>
+                            <td>"071-0117456"</td>
+                            <td>"No 424/4,Kuttigala,Padalangala"</td>
+                            <td>"Name"</td>
+                            <td>"Active"</td>
+                            <td>"Date"</td>
+                            <td>Red Roses<br>Yellow Roses<br>Pink Roses</td>
+                            <td>2.30 hours</td>
+                            <td>
+                     <button class='btn btn-danger btn-sm delete btn-flat' data-id=''><i class='fa fa-trash'></i> Delete</button>
+                            </td>
+                          </tr>
+                       
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+     
+  </div>
+  	<?php include 'includes/footer.php'; ?>
+    <?php include 'includes/users_modal.php'; ?>
+
+</div>
+<!-- ./wrapper -->
+
+<?php include 'includes/scripts.php'; ?>
+<script>
+$(function(){
+
+  $(document).on('click', '.edit', function(e){
+    e.preventDefault();
+    $('#edit').modal('show');
+    var id = $(this).data('id');
+    getRow(id);
+  });
+
+  $(document).on('click', '.delete', function(e){
+    e.preventDefault();
+    $('#delete').modal('show');
+    var id = $(this).data('id');
+    getRow(id);
+  });
+
+  $(document).on('click', '.photo', function(e){
+    e.preventDefault();
+    var id = $(this).data('id');
+    getRow(id);
+  });
+
+  $(document).on('click', '.status', function(e){
+    e.preventDefault();
+    var id = $(this).data('id');
+    getRow(id);
+  });
+
+});
+
+function getRow(id){
+  $.ajax({
+    type: 'POST',
+    url: 'users_row.php',
+    data: {id:id},
+    dataType: 'json',
+    success: function(response){
+      $('.userid').val(response.id);
+      $('#edit_email').val(response.email);
+      $('#edit_password').val(response.password);
+      $('#edit_firstname').val(response.firstname);
+      $('#edit_lastname').val(response.lastname);
+      $('#edit_address').val(response.address);
+      $('#edit_contact').val(response.contact_info);
+      $('.fullname').html(response.firstname+' '+response.lastname);
+    }
+  });
+}
+</script>
+</body>
+</html>
